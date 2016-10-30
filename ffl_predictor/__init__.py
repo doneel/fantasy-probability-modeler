c661e0f17c3eb690e7fd3610306a93e53740b259
danielon <danielon@wealthfront.com>
2016-10-30 14:54:59 -0700

    testing harness

--- ffl_predictor/__init__.py
+++ ffl_predictor/__init__.py
@@ -2,3 +2,4 @@ from .team_summarizer import mean_var_scores
 from .season import Season
 from .csv_parser import read_scores
 from .csv_parser import read_schedule
+from .season_simulator import SeasonSimulator
