5bd5ad914bff6b34f46523857c8f5c5ae32febd3
danielon <danielon@wealthfront.com>
2016-11-03 08:49:31 -0700

    converting to use indices on dataframes

--- ffl_predictor/individual_normal_season_simulator.py
+++ ffl_predictor/individual_normal_season_simulator.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 from ffl_predictor import RegularSeasonSimulator
 from ffl_predictor import mean_var_scores
 
@@ -10,5 +12,45 @@ class IndividualNormalSeasonSimulator(RegularSeasonSimulator):
 
     def simulate(self):
         distrubution_params = mean_var_scores(self.season.scores)
-        print(distrubution_params)
+        print(self.season.scores)
+
+        new_frame = self.season.scores[['week', 'score']]
+        new_frame.index = self.season.scores['team']
+        print(new_frame)
+        return
+        print(self.season.scores[['week', 'score']])
+        print(pd.DataFrame(self.season.scores[['week', 'score']], index=self.season.scores['team'], columns=['week', 'score']))
         #TODO: Does append return a new array or same?
+
+    def scores_remaining_per_team(self):
+        """ Ok so to be fastest, I should just generate N std normals and then multiply / add them by the relevant team columns then what? Map these into a new dataframe and append ok smart"""
+        """ Another way to look at this is that I could do the simulations in batch: Generate 100000 of these results at one time by just duplicating the dataframe 100000 times and doing the same process, then splitting it back up into the right size.
+            That batch computation is probably only of limited use though. Not gonna worry about that optimization yet.
+        """
+        #self.season.scores.index = self.season.scores['team']
+        #self.season.scores = self.season.scores[['week', 'score']]
+        all_scheduled = (
+            self.season.schedule[['t1', 'week']]
+                .rename(columns={'t1': 'team'})
+                .append(self.season.schedule[['t2', 'week']]
+                            .rename(columns={'t2': 'team'}))
+                .merge(self.season.scores, on=['team', 'week'], how='outer')
+        )
+        all_scheduled.index = pd.MultiIndex.from_arrays(
+                [all_scheduled['team'], all_scheduled['week']])
+        all_scheduled = all_scheduled['score']
+        unplayed = all_scheduled.isnull()
+        combined_data = (mean_var_scores(self.season.scores)
+                         .join(all_scheduled[unplayed]))
+        combined_data['score'] = (
+                np.random.normal(size = unplayed.sum())
+                * np.sqrt(combined_data['var'])
+                + combined_data['mean'])
+        combined_data.index = pd.MultiIndex.from_arrays(
+            [combined_data['team'], combined_data['week']])
+        combined_data = combined_data['score']
+        print(all_scheduled[unplayed_2])
+        print(combined_data)
+        all_scheduled[unplayed_2] = combined_data
+        print(all_scheduled)
+        return all_scheduled
--- tests/test_individual_normal_season_simulator.py
+++ tests/test_individual_normal_season_simulator.py
@@ -1,6 +1,35 @@
 """ Unit test IndividualNormalSeasonSimulator """
+import pytest
+import pandas as pd
+import numpy as np
 from ffl_predictor import IndividualNormalSeasonSimulator
-from test_season_simulator import sample_season
+from ffl_predictor import Season
+
+@pytest.fixture()
+def sample_season():
+    scores = pd.DataFrame(np.array([
+            ('team1', 1, 1.0), ('team1', 2, 1.0),
+            ('team2', 1, 0.0), ('team2', 2, 10.0),
+            ('team3', 1, 1.0), ('team3', 2, 2.0),
+            ('team4', 1, 3.0), ('team4', 2, 4.0)],
+        dtype=[('team', np.str, 8), ('week', np.int), ('score', np.float)]))
+    schedule = pd.DataFrame(np.array([
+            ('team1', 'team2', 1),
+            ('team3', 'team4', 1),
+            ('team1', 'team3', 2),
+            ('team2', 'team4', 2),
+            ('team1', 'team4', 3),
+            ('team2', 'team3', 3),
+            ('team1', 'team3', 4),
+            ('team2', 'team4', 4)],
+        dtype=[('t1', np.str, 8), ('t2', np.str, 8), ('week', np.int)]))
+    return Season(schedule, scores)
+
 
 def test_simulate(sample_season):
-    IndividualNormalSeasonSimulator(sample_season).simulate()
+    #print(sample_season.scores)
+
+    #IndividualNormalSeasonSimulator(sample_season).simulate()
+    IndividualNormalSeasonSimulator(sample_season).scores_remaining_per_team()
+    #TODO figure out a way to seed the random number generator so I can assert on the actual random values it produces
+    assert 1 == 2
